e6c1f1d2f132a23e147e06b69eab9364f7353c4b
DESKTOP-PU3H7CP\YAREN <cansusariiii@outlook.com>
2020-11-28 20:46:15 +0300

    +add: property to lesson

---
+++ CompecLesson/Assets/Scripts/LessonsBasic/FirstLessonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonsBasic 
+    
+{
+  public class FirstLessonScript : MonoBehaviour
+  {
+    // Start is called before the first frame update
+    // float : ondalık
+    [SerializeField] int Number3 = 5;
+        [SerializeField] private Vector3 vector3 = new Vector3(1, 2, 3);
+
+       
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  }
+}
