2fc44a0ba013f62a40320bbd6770ac24b296cd20
DESKTOP-PU3H7CP\YAREN <cansusariiii@outlook.com>
2020-11-28 20:50:05 +0300

    +created: rotate settings +refactor: rotate connected to settings

---
+++ CompecLesson/Assets/Scripts/Lessons/RotateObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+namespace Lessons 
+{ 
+
+public class RotateObject : MonoBehaviour
+    {
+        
+        [SerializeField] private RotateSettings rotateSettings;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, rotateSettings.Speed *Time.deltaTime);
+        }
+    }
+}
+
+
+
