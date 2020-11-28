38ed2a1420fdd1877a66c29824fdc533945f7ec5
DESKTOP-PU3H7CP\YAREN <cansusariiii@outlook.com>
2020-11-28 16:21:24 +0300

    +created first script lesson

---
+++ CompecLesson/Assets/Scripts/FirstLessonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonBasic 
+    
+{
+  public class FirstLessonScript : MonoBehaviour
+  {
+    // Start is called before the first frame update
+    // float : ondalık
+    [SerializeField] int Number3 = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  }
+}
