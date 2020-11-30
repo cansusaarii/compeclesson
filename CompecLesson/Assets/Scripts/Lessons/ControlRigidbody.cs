29e04f8d72841b267c02838659d8e8c648af46ec
DESKTOP-PU3H7CP\YAREN <cansusariiii@outlook.com>
2020-11-30 21:59:56 +0300

    +basic physics

---
+++ CompecLesson/Assets/Scripts/Lessons/ControlRigidbody.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Lessons
+    {
+
+public class ControlRigidbody : MonoBehaviour
+   {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private ControlRigidbodySettings controlRigidbodySettings;
+        private void Update()
+        {
+            bool spaceKeyDown = Input.GetKeyDown(KeyCode.Space);
+            if (spaceKeyDown)
+            {
+                _rigidbody.AddForce(controlRigidbodySettings.JumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+    
+}
